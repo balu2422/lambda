12081a56f79386f95776d10e7253add3eb6e71c9
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-15 16:59:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "aws_ecr_repository" "lambda_repo" {
 
 resource "aws_lambda_function" "lambda" {
   function_name = "lambda-task-function"
+  package_type  = "Image"
   image_uri     = var.image_uri
   role          = aws_iam_role.lambda_execution_role.arn
   memory_size   = 128
@@ -32,6 +33,19 @@ resource "aws_iam_role" "lambda_execution_role" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
 resource "aws_api_gateway_rest_api" "api" {
   name        = "lambda-api"
   description = "API for Lambda function"
@@ -51,15 +65,21 @@ resource "aws_api_gateway_method" "method" {
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = aws_lambda_function.lambda.invoke_arn
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
+  depends_on  = [aws_api_gateway_integration.integration]
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
 }
